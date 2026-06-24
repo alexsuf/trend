fef8a778d06f1b7d29e3e2b465dae859b6ca5da8
alexsuf <aleksey.zadonsky@gmail.com>
2026-06-24 17:23:39 +0300

    05 Отдельная база trend

--- app/config.py
+++ app/config.py
@@ -7,3 +7,5 @@ class Config:
     KEYCLOAK_REALM = os.environ.get('KEYCLOAK_REALM') or 'trend'
     KEYCLOAK_CLIENT_ID = os.environ.get('KEYCLOAK_CLIENT_ID') or 'trend-web'
     KEYCLOAK_CLIENT_SECRET = os.environ.get('KEYCLOAK_CLIENT_SECRET') or 'bbWGIugaSj9ithjybqoNR5hXI9acjEel'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://trend:secret@postgres.keycloak.svc.cluster.local:5432/trend'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
